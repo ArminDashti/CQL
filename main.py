36fa9e775791ea4feb163641ee4474ef2ac118e2
Armin <arminonline71@gmail.com>
2024-05-24 15:36:18 +0330

    Update

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+# From https://github.com/aviralkumar2907/CQL/blob/master/d4rl/rlkit/torch/sac/cql.py
+
 import torch
 import torch.nn nn
+
+
 
 
 
--- model.py
+++ model.py
@@ -23,35 +23,27 @@ class CQLTrainer:
             qf2,
             target_qf1,
             target_qf2,
-
             discount=0.99,
             reward_scale=1.0,
-
             policy_lr=1e-3,
             qf_lr=1e-3,
             optimizer_class=optim.Adam,
-
             soft_target_tau=1e-2,
             plotter=None,
             render_eval_paths=False,
-
             use_automatic_entropy_tuning=True,
             target_entropy=None,
             policy_eval_start=0,
             num_qs=2,
-
-            # CQL
             min_q_version=3,
             temp=1.0,
             min_q_weight=1.0,
-
-            ## sort of backup
             max_q_backup=False,
             deterministic_backup=True,
             num_random=10,
             with_lagrange=False,
-            lagrange_thresh=0.0,
-    ):
+            lagrange_thresh=0.0):
+        
         super().__init__()
         self.env = env
         self.policy = policy
@@ -68,19 +60,13 @@ class CQLTrainer:
             else:
                 self.target_entropy = -np.prod(self.env.action_space.shape).item() 
             self.log_alpha = ptu.zeros(1, requires_grad=True)
-            self.alpha_optimizer = optimizer_class(
-                [self.log_alpha],
-                lr=policy_lr,
-            )
+            self.alpha_optimizer = optimizer_class([self.log_alpha], lr=policy_lr)
         
         self.with_lagrange = with_lagrange
         if self.with_lagrange:
             self.target_action_gap = lagrange_thresh
             self.log_alpha_prime = ptu.zeros(1, requires_grad=True)
-            self.alpha_prime_optimizer = optimizer_class(
-                [self.log_alpha_prime],
-                lr=qf_lr,
-            )
+            self.alpha_prime_optimizer = optimizer_class([self.log_alpha_prime], lr=qf_lr)
 
         self.plotter = plotter
         self.render_eval_paths = render_eval_paths
@@ -88,18 +74,9 @@ class CQLTrainer:
         self.qf_criterion = nn.MSELoss()
         self.vf_criterion = nn.MSELoss()
 
-        self.policy_optimizer = optimizer_class(
-            self.policy.parameters(),
-            lr=policy_lr,
-        )
-        self.qf1_optimizer = optimizer_class(
-            self.qf1.parameters(),
-            lr=qf_lr,
-        )
-        self.qf2_optimizer = optimizer_class(
-            self.qf2.parameters(),
-            lr=qf_lr,
-        )
+        self.policy_optimizer = optimizer_class(self.policy.parameters(), lr=policy_lr)
+        self.qf1_optimizer = optimizer_class(self.qf1.parameters(), lr=qf_lr)
+        self.qf2_optimizer = optimizer_class(self.qf2.parameters(), lr=qf_lr)
 
         self.discount = discount
         self.reward_scale = reward_scale
@@ -107,28 +84,20 @@ class CQLTrainer:
         self._n_train_steps_total = 0
         self._need_to_update_eval_statistics = True
         self.policy_eval_start = policy_eval_start
-        
         self._current_epoch = 0
         self._policy_update_ctr = 0
         self._num_q_update_steps = 0
         self._num_policy_update_steps = 0
         self._num_policy_steps = 1
-        
         self.num_qs = num_qs
-
-        ## min Q
         self.temp = temp
         self.min_q_version = min_q_version
         self.min_q_weight = min_q_weight
-
         self.softmax = torch.nn.Softmax(dim=1)
         self.softplus = torch.nn.Softplus(beta=self.temp, threshold=20)
-
         self.max_q_backup = max_q_backup
         self.deterministic_backup = deterministic_backup
         self.num_random = num_random
-
-        # For implementation on the 
         self.discrete = False
     
     
@@ -149,9 +118,7 @@ class CQLTrainer:
 
     def _get_policy_actions(self, obs, num_actions, network=None):
         obs_temp = obs.unsqueeze(1).repeat(1, num_actions, 1).view(obs.shape[0] * num_actions, obs.shape[1])
-        new_obs_actions, _, _, new_obs_log_pi, *_ = network(
-            obs_temp, reparameterize=True, return_log_prob=True,
-        )
+        new_obs_actions, _, _, new_obs_log_pi, *_ = network(obs_temp, reparameterize=True, return_log_prob=True)
         if not self.discrete:
             return new_obs_actions, new_obs_log_pi.view(obs.shape[0], num_actions, 1)
         else:
@@ -164,13 +131,7 @@ class CQLTrainer:
         obs = batch['observations']
         actions = batch['actions']
         next_obs = batch['next_observations']
-
-        """
-        Policy and Alpha Loss
-        """
-        new_obs_actions, policy_mean, policy_log_std, log_pi, *_ = self.policy(
-            obs, reparameterize=True, return_log_prob=True,
-        )
+        new_obs_actions, policy_mean, policy_log_std, log_pi, *_ = self.policy(obs, reparameterize=True, return_log_prob=True)
         
         if self.use_automatic_entropy_tuning:
             alpha_loss = -(self.log_alpha * (log_pi + self.target_entropy).detach()).mean()
@@ -185,50 +146,31 @@ class CQLTrainer:
         if self.num_qs == 1:
             q_new_actions = self.qf1(obs, new_obs_actions)
         else:
-            q_new_actions = torch.min(
-                self.qf1(obs, new_obs_actions),
-                self.qf2(obs, new_obs_actions),
-            )
+            q_new_actions = torch.min(self.qf1(obs, new_obs_actions), self.qf2(obs, new_obs_actions))
 
         policy_loss = (alpha*log_pi - q_new_actions).mean()
 
         if self._current_epoch < self.policy_eval_start:
-            """
-            For the initial few epochs, try doing behaivoral cloning, if needed
-            conventionally, there's not much difference in performance with having 20k 
-            gradient steps here, or not having it
-            """
             policy_log_prob = self.policy.log_prob(obs, actions)
             policy_loss = (alpha * log_pi - policy_log_prob).mean()
-        
-        """
-        QF Loss
-        """
+
         q1_pred = self.qf1(obs, actions)
         if self.num_qs > 1:
             q2_pred = self.qf2(obs, actions)
         
-        new_next_actions, _, _, new_log_pi, *_ = self.policy(
-            next_obs, reparameterize=True, return_log_prob=True,
-        )
-        new_curr_actions, _, _, new_curr_log_pi, *_ = self.policy(
-            obs, reparameterize=True, return_log_prob=True,
-        )
+        new_next_actions, _, _, new_log_pi, *_ = self.policy(next_obs, reparameterize=True, return_log_prob=True)
+        new_curr_actions, _, _, new_curr_log_pi, *_ = self.policy(obs, reparameterize=True, return_log_prob=True)
 
         if not self.max_q_backup:
             if self.num_qs == 1:
                 target_q_values = self.target_qf1(next_obs, new_next_actions)
             else:
-                target_q_values = torch.min(
-                    self.target_qf1(next_obs, new_next_actions),
-                    self.target_qf2(next_obs, new_next_actions),
-                )
+                target_q_values = torch.min(self.target_qf1(next_obs, new_next_actions), self.target_qf2(next_obs, new_next_actions))
             
             if not self.deterministic_backup:
                 target_q_values = target_q_values - alpha * new_log_pi
         
         if self.max_q_backup:
-            """when using max q backup"""
             next_actions_temp, _ = self._get_policy_actions(next_obs, num_actions=10, network=self.policy)
             target_qf1_values = self._get_tensor_values(next_obs, next_actions_temp, network=self.target_qf1).max(1)[0].view(-1, 1)
             target_qf2_values = self._get_tensor_values(next_obs, next_actions_temp, network=self.target_qf2).max(1)[0].view(-1, 1)
@@ -241,8 +183,7 @@ class CQLTrainer:
         if self.num_qs > 1:
             qf2_loss = self.qf_criterion(q2_pred, q_target)
 
-        ## add CQL
-        random_actions_tensor = torch.FloatTensor(q2_pred.shape[0] * self.num_random, actions.shape[-1]).uniform_(-1, 1) # .cuda()
+        random_actions_tensor = torch.FloatTensor(q2_pred.shape[0] * self.num_random, actions.shape[-1]).uniform_(-1, 1)
         curr_actions_tensor, curr_log_pis = self._get_policy_actions(obs, num_actions=self.num_random, network=self.policy)
         new_curr_actions_tensor, new_log_pis = self._get_policy_actions(next_obs, num_actions=self.num_random, network=self.policy)
         q1_rand = self._get_tensor_values(obs, random_actions_tensor, network=self.qf1)
@@ -252,29 +193,19 @@ class CQLTrainer:
         q1_next_actions = self._get_tensor_values(obs, new_curr_actions_tensor, network=self.qf1)
         q2_next_actions = self._get_tensor_values(obs, new_curr_actions_tensor, network=self.qf2)
 
-        cat_q1 = torch.cat(
-            [q1_rand, q1_pred.unsqueeze(1), q1_next_actions, q1_curr_actions], 1
-        )
-        cat_q2 = torch.cat(
-            [q2_rand, q2_pred.unsqueeze(1), q2_next_actions, q2_curr_actions], 1
-        )
+        cat_q1 = torch.cat([q1_rand, q1_pred.unsqueeze(1), q1_next_actions, q1_curr_actions], 1)
+        cat_q2 = torch.cat([q2_rand, q2_pred.unsqueeze(1), q2_next_actions, q2_curr_actions], 1)
         std_q1 = torch.std(cat_q1, dim=1)
         std_q2 = torch.std(cat_q2, dim=1)
 
         if self.min_q_version == 3:
-            # importance sammpled version
             random_density = np.log(0.5 ** curr_actions_tensor.shape[-1])
-            cat_q1 = torch.cat(
-                [q1_rand - random_density, q1_next_actions - new_log_pis.detach(), q1_curr_actions - curr_log_pis.detach()], 1
-            )
-            cat_q2 = torch.cat(
-                [q2_rand - random_density, q2_next_actions - new_log_pis.detach(), q2_curr_actions - curr_log_pis.detach()], 1
-            )
+            cat_q1 = torch.cat([q1_rand - random_density, q1_next_actions - new_log_pis.detach(), q1_curr_actions - curr_log_pis.detach()], 1)
+            cat_q2 = torch.cat([q2_rand - random_density, q2_next_actions - new_log_pis.detach(), q2_curr_actions - curr_log_pis.detach()], 1)
             
         min_qf1_loss = torch.logsumexp(cat_q1 / self.temp, dim=1,).mean() * self.min_q_weight * self.temp
         min_qf2_loss = torch.logsumexp(cat_q2 / self.temp, dim=1,).mean() * self.min_q_weight * self.temp
                     
-        """Subtract the log likelihood of data"""
         min_qf1_loss = min_qf1_loss - q1_pred.mean() * self.min_q_weight
         min_qf2_loss = min_qf2_loss - q2_pred.mean() * self.min_q_weight
         
@@ -291,10 +222,6 @@ class CQLTrainer:
         qf1_loss = qf1_loss + min_qf1_loss
         qf2_loss = qf2_loss + min_qf2_loss
 
-        """
-        Update networks
-        """
-        # Update the Q-functions iff 
         self._num_q_update_steps += 1
         self.qf1_optimizer.zero_grad()
         qf1_loss.backward(retain_graph=True)
@@ -310,25 +237,12 @@ class CQLTrainer:
         policy_loss.backward(retain_graph=False)
         self.policy_optimizer.step()
 
-        """
-        Soft Updates
-        """
-        ptu.soft_update_from_to(
-            self.qf1, self.target_qf1, self.soft_target_tau
-        )
+        ptu.soft_update_from_to(self.qf1, self.target_qf1, self.soft_target_tau)
+        
         if self.num_qs > 1:
-            ptu.soft_update_from_to(
-                self.qf2, self.target_qf2, self.soft_target_tau
-            )
-        """
-        Save some statistics for eval
-        """
+            ptu.soft_update_from_to(self.qf2, self.target_qf2, self.soft_target_tau)
         if self._need_to_update_eval_statistics:
             self._need_to_update_eval_statistics = False
-            """
-            Eval should set this to None.
-            This way, these statistics are only computed for one batch.
-            """
             policy_loss = (log_pi - q_new_actions).mean()
 
             self.eval_statistics['QF1 Loss'] = np.mean(ptu.get_numpy(qf1_loss))
